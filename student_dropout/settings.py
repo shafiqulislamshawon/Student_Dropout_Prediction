81119e9e135e0eb5c40a1c2d6f4ac371f4d3ee60
SK-Subroto <subroto35-1734@diu.edu.bd>
2022-02-15 13:09:26 +0600

    install required file for deploy

--- student_dropout/settings.py
+++ student_dropout/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'ravq(tani(@d1n^qzgobtdd%&bii(2d__6emad*ht2^1si3yvn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['student-dropout-sk.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -76,13 +76,20 @@ WSGI_APPLICATION = 'student_dropout.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'student_dropout_prediction',
+#         'USER': 'postgres',
+#         'PASSWORD': '210048774',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'student_dropout_prediction',
-        'USER': 'postgres',
-        'PASSWORD': '210048774',
-        'HOST': 'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
